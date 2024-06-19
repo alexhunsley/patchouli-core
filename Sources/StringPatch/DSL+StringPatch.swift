205a6773464b9be51ae41e85fa5c692d605d5021
Alex Hunsley <alex.hunsley@gmail.com>
2024-06-19 20:54:46 +0100

    Fix comment

--- Sources/StringPatch/DSL+StringPatch.swift
+++ Sources/StringPatch/DSL+StringPatch.swift
@@ -7,11 +7,11 @@
 
 import Foundation
 
-/// Conveience for string patcher's test method that doesn't require an expected content param
-/// (the address is all we need, we're checking to see if it's in the string)
+/// Conveience for string patcher's test method that doesn't require an address param
+/// (the expected content is all we need, we're checking to see if it's in the string)
 public func Test(expectedContent: String) -> AddressedPatch<StringPatchType> {
     // Note we give expectedContent for the address as well as the expectedContent,
-    // as it's required for that case (but not used here)
+    // as it's required for this patcher func (but not used)
     return AddressedPatch(patchSpec: .test(expectedContent, expectedContent),
                           contentPatch: PatchedContent<StringPatchType>(content: expectedContent))
 }
